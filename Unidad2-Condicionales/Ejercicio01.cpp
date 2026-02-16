dc4f487ad98d338da65505444d3212c8d4bd84d0
Tu Nombre de Usuario <mika.villagra09@gmail.com>
2026-02-16 19:46:16 -0300

    Unidad 2 - ejercicio 1

---
+++ Unidad2-Condicionales/Ejercicio01.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int num;
+
+    cout<< "Ingrese un numero" << endl;
+
+    cin >> num;
+
+    if (num > 0)
+    {
+        cout<< "El numero es positivo" <<endl;
+    }
+    else if (num < 0)
+    {
+        cout<< "El numero es negativo." <<endl;
+    }
+    else
+    {
+        cout<< "El numero es cero." << endl;
+    }
+
+
+    return 0;
+}
