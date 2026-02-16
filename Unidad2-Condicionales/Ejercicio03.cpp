1593535bafb9a959837d6e8c08a99cae6f4d9780
Tu Nombre de Usuario <mika.villagra09@gmail.com>
2026-02-16 20:16:20 -0300

    Unidad 2 - Ejercicio 3

---
+++ Unidad2-Condicionales/Ejercicio03.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout<<"Ingrese un numero : " <<endl;
+    cin>> num;
+
+    if ((num % 2 ) == 0)
+    {
+        cout<<"El numero es par." << endl;
+    }
+    else
+    {
+        cout<<"El numero es impar."<< endl;
+    }
+
+
+
+
+    return 0;
+}
